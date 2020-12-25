9c1534c1cb1847e44b853514eef5726b495a2437
afroz-in <kingkhanafroz517@gmail.com>
2020-12-25 23:45:32 +0530

    Updates

---
+++ precache-manifest.9ed3796db16b6dcb11f79edfda10a680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b50b944566fc47848ca21b5498c4f8",
+    "url": "/React-TODO-List/index.html"
+  },
+  {
+    "revision": "9923e91cda570cc21f2f",
+    "url": "/React-TODO-List/static/css/main.9e2546c0.chunk.css"
+  },
+  {
+    "revision": "cf72b4ddabf18774ab0f",
+    "url": "/React-TODO-List/static/js/2.0b535ef0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-TODO-List/static/js/2.0b535ef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9923e91cda570cc21f2f",
+    "url": "/React-TODO-List/static/js/main.8bc78b31.chunk.js"
+  },
+  {
+    "revision": "3d72c79373d76386b207",
+    "url": "/React-TODO-List/static/js/runtime-main.916df8c0.js"
+  }
+]);
